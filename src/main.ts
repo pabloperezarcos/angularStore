c8e94036e5b002721249eb8e16f62df74389e199
pabloperezarcos <pabloperezarcos@gmail.com>
2024-11-18 23:05:06 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
 import { registerLocaleData } from '@angular/common';
 import localeEsCL from '@angular/common/locales/es-CL';
 
-// Registro de localización
+/**
+ * Registra los datos de localización para el idioma español de Chile (es-CL).
+ */
 registerLocaleData(localeEsCL, 'es-CL');
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes)
-  ]
-}).catch(err => console.error(err));
+/**
+ * Inicializa la aplicación Angular utilizando la configuración especificada y el componente raíz.
+ * Muestra un error en la consola si la inicialización falla.
+ */
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
