da18598ee74079f981f2e6c0bf5067e5021722d0
pabloperezarcos <pabloperezarcos@gmail.com>
2024-12-14 12:43:15 -0300

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -4,6 +4,11 @@ import {
     BrowserDynamicTestingModule,
     platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
+import { registerLocaleData } from '@angular/common';
+import localeEsCL from '@angular/common/locales/es-CL';
+
+// Registrar datos de localización para 'es-CL'
+registerLocaleData(localeEsCL);
 
 // Inicializar el entorno de pruebas Angular
 getTestBed().initTestEnvironment(
